002ba19daf8a31667e76a29955f012632a6e5e65
shubham kumar <18shubham1998@gmail.com>
2022-05-01 12:17:43 +0530

    solve subrectangle_queries

--- Subrectangle_Queries.cpp
+++ Subrectangle_Queries.cpp
@@ -6,3 +6,38 @@ Updates all values with newValue in the subrectangle whose upper left coordinate
 2. getValue(int row, int col)
 
 Returns the current value of the coordinate (row,col) from the rectangle. */
+
+/* Example:
+
+
+
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class SubrectangleQueries
+{
+    vector<vector<int>> rect;
+
+public:
+    SubrectangleQueries(vector<vector<int>> &rectangle)
+    {
+        rect = rectangle;
+    }
+    void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue)
+    {
+        for (size_t r = row1; r <= row2; r++)
+        {
+            for (size_t c = col1; c <= col2; c++)
+            {
+                rect[r][c] = newValue;
+            }
+        }
+    }
+    int getValue(int row, int col)
+    {
+        return rect[row][col]
+    }
+};
