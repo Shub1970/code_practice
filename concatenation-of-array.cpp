0d1e8b8db939a693b12474deade76b8885a4cb5c
shubham kumar <18shubham1998@gmail.com>
2022-05-03 11:17:27 +0530

    complete concatenation of array

--- concatenation-of-array.cpp
+++ concatenation-of-array.cpp
@@ -4,7 +4,7 @@ Specifically, ans is the concatenation of two nums arrays.
 
 Return the array ans.
 
- 
+
 
 Example 1:
 
@@ -14,3 +14,36 @@ Explanation: The array ans is formed as follows:
 - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
 - ans = [1,2,1,1,2,1]
 */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> getConatenation(vector<int> &nums)
+    {
+        const int siz = nums.size();
+        vector<int> ans(2 * siz);
+        vector<int>::iterator p;
+        int i = 0;
+        for (p = nums.begin(); p != nums.end(); p++)
+        {
+            ans[i] = ans[siz + i] = *p;
+            ++i;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    vector<int> num{1, 2, 1};
+    Solution s;
+    vector<int> ans = s.getConatenation(num);
+    for (auto x : ans)
+    {
+        cout << x << " ";
+    }
+}
