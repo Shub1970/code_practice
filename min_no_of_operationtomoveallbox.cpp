345d3b28e8b88cda3dd454cd8dc2dd3e8fbb34e6
shubham kumar <41205124+Shub1970@users.noreply.github.com>
2022-05-03 12:39:02 +0530

    add optemize

--- min_no_of_operationtomoveallbox.cpp
+++ min_no_of_operationtomoveallbox.cpp
@@ -49,6 +49,32 @@ public:
     }
 };
 
+//opetimize
+/*
+Optimized
+class Solution {
+public:
+    vector<int> ans ;
+    vector<int> minOperations(string boxes) {
+        int ones = 0 ;
+        int temp = 0 ; 
+        for(int i = 0 ; i < boxes.size() ; ++i ){
+            ans.push_back(temp) ;
+            ones += boxes[i]-'0' ;
+            temp += ones ;
+        }
+        ones = temp = 0 ;
+        for(int i = boxes.size() - 1 ; i >=0 ; --i ){
+            ans[i] += temp ;;
+            ones += boxes[i]-'0' ;
+            temp += ones ;
+        }
+        return ans ;
+        
+    }
+};
+*/
+
 int main()
 {
     string st = "001011";
