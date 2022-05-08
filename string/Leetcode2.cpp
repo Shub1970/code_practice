1c156ff9983d2ae6d87498b9e6915ab6939c880c
shubham kumar <18shubham1998@gmail.com>
2022-05-08 15:19:36 +0530

    solve Leetcode2

--- string/Leetcode2.cpp
+++ string/Leetcode2.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <math.h>
 using namespace std;
+/*
 int getvalue(string word)
 {
     int size = word.size();
@@ -26,8 +27,24 @@ public:
         int targetWordSum = getvalue(targetWord);
         return firstWordSum + secondWordSum == targetWordSum;
     }
-};
+}; */
+//---------------------------------------------------------------------------------------------------------------------
+// better solution
 
+class Solution
+{
+public:
+    bool isSumEqual(string firstWord, string secondWord, string targetWord)
+    {
+        for (char &c : firstWord)
+            c -= 'a' - '0';
+        for (char &c : secondWord)
+            c -= 'a' - '0';
+        for (char &c : targetWord)
+            c -= 'a' - '0';
+        return stoi(firstWord) + stoi(secondWord) == stoi(targetWord);
+    }
+};
 int main()
 {
     Solution s;
