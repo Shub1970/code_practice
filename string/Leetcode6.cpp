4443ae18156834383d4439f68bfa22c78dd41d3c
shubham kumar <18shubham1998@gmail.com>
2022-05-09 13:33:59 +0530

    solve bug

--- string/Leetcode6.cpp
+++ string/Leetcode6.cpp
@@ -7,6 +7,10 @@ class Solution
 public:
     int countGoodSubstrings(string s)
     {
+        if (s.size() < 3)
+        {
+            return 0;
+        }
         int count = 0;
         for (int i = 0; i < s.size() - 2; i++)
         {
@@ -21,7 +25,7 @@ public:
 int main()
 {
     Solution s;
-    string s1 = "AABABCABC";
+    string s1 = "A";
     cout << s.countGoodSubstrings(s1);
     return 0;
 }
