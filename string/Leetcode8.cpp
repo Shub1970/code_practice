cbb0e832c26b4968583f5069b03b0c9351d8ef5d
shubham kumar <18shubham1998@gmail.com>
2022-05-10 19:14:53 +0530

    add better solution to Leetcode8

--- string/Leetcode8.cpp
+++ string/Leetcode8.cpp
@@ -70,6 +70,33 @@ public:
     }
 };
 
+// better solution
+class Solution
+{
+public:
+    bool areNumbersAscending(string s)
+    {
+        int prev = 0;
+        int curr = 0;
+        for (auto ch : s)
+        {
+            if (isdigit(ch))
+            {
+                curr = curr * 10 + ch - '0';
+            }
+            else if (curr != 0)
+            {
+                if (prev >= curr)
+                {
+                    return false;
+                }
+                prev = curr;
+                curr = 0;
+            }
+        }
+        return curr == 0 || prev < curr;
+    }
+};
 int main()
 {
     Solution s;
