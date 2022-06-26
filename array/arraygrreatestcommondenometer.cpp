31e220fe7f33996b5706649ab5a8e0d694a1d516
shubham kumar <18shubham1998@gmail.com>
2022-06-26 10:29:01 +0530

    GCD update

--- array/arraygrreatestcommondenometer.cpp
+++ array/arraygrreatestcommondenometer.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-class Solution
+/* class Solution
 {
 public:
     int findGCD(vector<int> &nums)
@@ -28,6 +28,28 @@ public:
             }
         }
         return gcd;
+    }
+}; */
+// better way using euclidean algo.
+class Solution // elucid algo......
+{
+    int gcd(int a, int b)
+    {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+    int findGCD(vector<int> &nums)
+    {
+        int min = 1001, max = 0;
+        for (int i : nums)
+        {
+            if (min > i)
+                min = i;
+            if (max < i)
+                max = i;
+        }
+        return gcd(min, max);
     }
 };
 int main()
