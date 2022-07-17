386ab80aaee591f69e64dc7f483223462f363f4c
Shub1970 <18shubham1998@gmail.com>
2022-07-17 17:43:11 +0530

    solve leetcode7

--- sliding_window/leetcode8.cpp
+++ sliding_window/leetcode8.cpp
@@ -1,7 +1,36 @@
-#
-class Solution {
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
 public:
-    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
-        
+    int numberOfSubarrays(vector<int> &nums, int k)
+    {
+        int nice_subarray = 0;
+        for (int i = 0, start = 0, count_odd = 0; i < nums.size(); i++)
+        {
+            if (nums[i] % 2)
+                count_odd++;
+            if (count_odd == k)
+                nice_subarray++;
+            while (count_odd == k)
+            {
+                int k = 0;
+                if (nums[i] % 2)
+                    count_odd--;
+                start++;
+            }
+        }
+        return nice_subarray;
     }
 };
+
+int main()
+{
+    vector<int> nums{1, 1, 2, 1, 1};
+    int k = 3;
+    Solution obj;
+    cout << obj.numberOfSubarrays(nums, k) << endl;
+}
