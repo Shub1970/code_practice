26362dc6bbcbd9676a1574264af4408408b57aea
Shub1970 <18shubham1998@gmail.com>
2022-07-23 22:47:06 +0530

    solve leetcode5

--- heap/leetcode5.cpp
+++ heap/leetcode5.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> maxSubsequence(vector<int>& nums, int k) {
-        
+    vector<int> maxSubsequence(vector<int> &nums, int k)
+    {
+        int ans = 0;
+        priority_queue<pair<int, int>> pq;
+        priority_queue<pair<int, int>> qp;
+        for (int i = 1; i <= nums.size(); i++)
+        {
+            pq.push({nums[i - 1], -i});
+        }
+        vector<int> ret;
+        int K = k;
+        while (K--)
+        {
+            pair<int, int> temp = pq.top();
+            pq.pop();
+            qp.push({temp.second, temp.first});
+        }
+        while (k--)
+        {
+            ret.push_back(qp.top().second);
+            qp.pop();
+        }
+        return ret;
     }
 };
+
+int main()
+{
+    vector<int> nums{-1, -2, 3, 4};
+    Solution obj;
+    vector<int> ans = obj.maxSubsequence(nums, 3);
+    for (auto x : ans)
+        cout << x << " ";
+}
