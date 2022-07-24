74ce779ee33f766c462a764d64436b1c4b7210bb
Shub1970 <18shubham1998@gmail.com>
2022-07-24 23:35:31 +0530

    solve

--- weeklyContest/weekly302.cpp
+++ weeklyContest/weekly302.cpp
@@ -1,38 +1,80 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <set>
+#include <functional>
+#include <queue>
 using namespace std;
 // maximum Number of pairs in array
+// class Solution
+// {
+// public:
+//     vector<int> numberOfPairs(vector<int> &nums)
+//     {
+//         unordered_map<int, int> mp;
+//         for (auto x : nums)
+//         {
+//             mp[x]++;
+//         }
+//         int ans = 0;
+//         for (auto m : mp)
+//         {
+//             if (m.second & 1)
+//                 ans += (m.second - 1) / 2;
+//             else
+//                 ans += m.second / 2;
+//         }
+//         int second = nums.size() - ans * 2;
+//         return {ans, second};
+//     }
+// };
+
+// int main()
+// {
+//     vector<int> nums{1, 1};
+//     Solution obj;
+//     vector<int> ans = obj.numberOfPairs(nums);
+//     for (auto x : ans)
+//     {
+//         cout << x << " ";
+//     }
+// }
+
+// max sum of a pair with Equal sum of digits
+
 class Solution
 {
 public:
-    vector<int> numberOfPairs(vector<int> &nums)
+    int maximumSum(vector<int> &nums)
     {
-        unordered_map<int, int> mp;
+        unordered_map<int, priority_queue<int>> mp;
         for (auto x : nums)
         {
-            mp[x]++;
+            string temp = to_string(x);
+            int tem = 0;
+            for (auto ch : temp)
+                tem += (ch - '0');
+            mp[tem].push(x);
         }
-        int ans = 0;
+        int max = INT32_MIN;
         for (auto m : mp)
         {
-            if (m.second & 1)
-                ans += (m.second - 1) / 2;
-            else
-                ans += m.second / 2;
+            if (m.second.size() > 1)
+            {
+                int first{m.second.top()};
+                m.second.pop();
+                int second{m.second.top()};
+                if (max < first + second)
+                    max = first + second;
+            }
         }
-        int second = nums.size() - ans * 2;
-        return {ans, second};
+        return max;
     }
 };
 
 int main()
 {
-    vector<int> nums{1, 1};
+    vector<int> nums{18, 43, 36, 13, 7};
     Solution obj;
-    vector<int> ans = obj.numberOfPairs(nums);
-    for (auto x : ans)
-    {
-        cout << x << " ";
-    }
+    cout << obj.maximumSum(nums);
 }
