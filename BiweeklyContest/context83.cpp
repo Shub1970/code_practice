70853dc7441dfe668913d89b7bf8be16f5516033
Shub1970 <18shubham1998@gmail.com>
2022-07-24 11:39:26 +0530

    test

--- BiweeklyContest/context83.cpp
+++ BiweeklyContest/context83.cpp
@@ -25,32 +25,62 @@ using namespace std;
 //     }
 // };
 
+// class Solution
+// {
+// public:
+//     long long zeroFilledSubarray(vector<int> &nums)
+//     {
+//         stack<int, vector<int>> stac(nums);
+//         unordered_map<int, int> mp;
+//         int index = 0;
+//         while (!stac.empty())
+//         {
+//             if (!stac.top())
+//             {
+//                 while (!stac.top() && !stac.empty())
+//                 {
+//                     mp[index]++;
+//                     stac.pop();
+//                 }
+//                 index++;
+//             }
+//             else
+//                 stac.pop();
+//         }
+//         long long ans{0};
+//         for (auto x : mp)
+//         {
+//             ans += x.second * (x.second + 1) / 2;
+//         }
+//         return ans;
+//     }
+// };
+// problem is if stack size is very big
+
+// better solution
 class Solution
 {
 public:
     long long zeroFilledSubarray(vector<int> &nums)
     {
-        stack<int, vector<int>> stac(nums);
-        unordered_map<int, int> mp;
-        int index = 0;
-        while (!stac.empty())
+        vector<int> zeroSize;
+        for (int i = 0; i < nums.size(); i++)
         {
-            if (!stac.top())
+            int temp = 0;
+            if (!nums[i])
             {
-                while (!stac.top() && !stac.empty())
+                while (!nums[i] && i < nums.size())
                 {
-                    mp[index]++;
-                    stac.pop();
+                    temp++;
+                    i++;
                 }
-                index++;
+                zeroSize.push_back(temp);
             }
-            else
-                stac.pop();
         }
         long long ans{0};
-        for (auto x : mp)
+        for (auto x : zeroSize)
         {
-            ans += x.second * (x.second + 1) / 2;
+            ans += x * (x + 1) / 2;
         }
         return ans;
     }
@@ -58,7 +88,7 @@ public:
 
 int main()
 {
-    vector<int> nums{0, 0, 0, 2, 0, 0};
+    vector<int> nums{1, 3, 0, 0, 2, 0, 0, 4};
     Solution obj;
     cout << obj.zeroFilledSubarray(nums);
 }
