b9245a4f34652dfb5805ee26b8bcf14121ab48c3
Shub1970 <18shubham1998@gmail.com>
2022-07-25 07:08:49 +0530

    solve 9

---
+++ heap/leetcode8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int maximumScore(int a,int b,int c){
+        
+    }
+};
---
+++ heap/leetcode9.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+    vector<vector<int>> matri;
+    int row{0}, coloum{0}, ans{-1};
+
+public:
+    int countlessthanx(int x)
+    {
+        int count{0};
+        for (int r{0}; r < row; r++)
+        {
+            int c{coloum - 1};
+            for (c; matri[r][c] > x; c--)
+                continue;
+            count += (c + 1);
+        }
+        return count;
+    }
+    int KthLargestValue(vector<vector<int>> &matrix, int k)
+    {
+        matri = move(matrix);
+        row = matri.size();
+        coloum = matri[0].size();
+        int left{matri[0][0]}, right{matri[row - 1][coloum - 1]};
+        while (left <= right)
+        {
+            int med{left + right >> 1};
+            if (countlessthanx(med) >= k)
+            {
+                ans = med;
+                right = med - 1;
+            }
+            else
+                left = med + 1;
+        }
+    }
+};
