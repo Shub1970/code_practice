147d2896837475f6716a885e1433fe27caf5d54c
Shub1970 <18shubham1998@gmail.com>
2022-09-27 20:47:36 +0530

    leetcode9 solve

--- hashtable/leetcode9.cpp
+++ hashtable/leetcode9.cpp
@@ -1,6 +1,31 @@
+// https://leetcode.com/problems/top-k-frequent-elements/
 #include <iostream>
+#include <vector>
+#include <map>
 using namespace std;
-int main()
+class Solution
 {
-    cout << "hello world";
-}
+public:
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+        vector<string> copy_string(strs);
+        for (int i{0}; i < strs.size(); i++)
+        {
+            strs[i] = sort(strs[i].begin(), strs[i].end());
+        }
+        map<string, vector<int>> mp;
+        for (int i{0}; i < strs.size(); i++)
+        {
+            mp[copy_string[i]].push_back(i);
+        }
+        vector<vector<string>> result;
+        for (auto m : mp)
+        {
+            vector<int> temp;
+            for (auto x : m.second)
+                temp.push_back(strs[i]);
+            result.push_back(temp);
+        }
+        return result;
+    }
+};
