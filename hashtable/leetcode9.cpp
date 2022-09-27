062a766666735c1d9198877f197e75159f9e8091
Shub1970 <18shubham1998@gmail.com>
2022-09-27 21:46:19 +0530

    better solution

--- hashtable/leetcode9.cpp
+++ hashtable/leetcode9.cpp
@@ -4,27 +4,55 @@
 #include <unordered_map>
 #include <algorithm>
 using namespace std;
+// class Solution
+// {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string> &strs)
+//     {
+//         vector<string> copy_string(strs);
+//         for (auto &sub_string : copy_string)
+//         {
+//             sort(sub_string.begin(), sub_string.end());
+//         }
+//         unordered_map<string, vector<string>> maap;
+//         for (int i{0}; i < strs.size(); i++)
+//         {
+//             maap[copy_string[i]].push_back(strs[i]);
+//         }
+//         vector<vector<string>> result;
+//         for (auto mp : maap)
+//         {
+//             result.push_back(mp.second);
+//         }
+//         return result;
+//     }
+// };
+// using counting sort (because string only contain lower-case alphabets)
+
 class Solution
 {
+private:
+    string strSort(string s)
+    {
+        int counter[26]{0};
+        for (char c : s)
+            counter[c - 'a']++;
+        string temp;
+        for (int c{0}; c < 26; c++)
+            temp += string(counter[c], c + 'a');
+        return temp;
+    }
+
 public:
     vector<vector<string>> groupAnagrams(vector<string> &strs)
     {
-        vector<string> copy_string(strs);
-        for (auto &sub_string : copy_string)
-        {
-            sort(sub_string.begin(), sub_string.end());
-        }
-        unordered_map<string, vector<string>> maap;
-        for (int i{0}; i < strs.size(); i++)
-        {
-            maap[copy_string[i]].push_back(strs[i]);
-        }
-        vector<vector<string>> result;
-        for (auto mp : maap)
-        {
-            result.push_back(mp.second);
-        }
-        return result;
+        unordered_map<string, vector<string>> mp;
+        for (string s : strs)
+            mp[strSort(s)].push_back(s);
+        vector<vector<string>> anagrams;
+        for (auto p : mp)
+            anagrams.push_back(p.second);
+        return anagrams;
     }
 };
 int main()
